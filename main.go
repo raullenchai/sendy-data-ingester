1407efa65690827930a29206a176946d65c0fe8b
lzxm160 <lzxm160@sina.com>
2019-07-30 14:00:02 +0800

    x

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/csv"
 	"github.com/lzxm160/csvtomysql/utils"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 type config struct {
 	MysqlConnectString string`json:"mysqlConnectString,omitempty"`
+	DbName string`json:"dbName,omitempty"`
+	TableName string`json:"tableName,omitempty"`
 }
+
 
 var(
 	configPath string
 	csvPath string
+	line uint64
+	sleepSeconds uint64
 )
 
 func init() {
@@ -24,12 +32,52 @@ func init() {
 	}
 	flag.StringVar(&configPath, "config", "config.toml", "path of config file")
 	flag.StringVar(&csvPath, "csv", "csv.csv", "path of csv file")
+	flag.Uint64Var(&line, "line",500000, "line")
+	flag.Uint64Var(&sleepSeconds, "sleep",3, "sleep")
 	flag.Parse()
+}
+func openDB(cfg config)(*sql.DB,error){
+	db, err := sql.Open("mysql", cfg.MysqlConnectString+cfg.DbName+"?autocommit=false")
+	if err != nil {
+		return nil,err
+	}
+	return db,nil
+}
+func readAndWrite(s *sql.DB){
+	f, err := os.Open(csvPath)
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	r := csv.NewReader(f)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		
+		fmt.Println(record)
+		fmt.Println(len(record))
+		for value := range record {
+			fmt.Printf("  %v\n", record[value])
+		}
+	}
 }
 func main() {
 	var cfg config
 	utils.LoadConfig(configPath,&cfg)
-	fmt.Println("::",cfg)
+	fmt.Println("config:",cfg)
+	db,err:=openDB(cfg)
+	defer db.Close()
 
-	//select {}
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	readAndWrite(db)
 }
