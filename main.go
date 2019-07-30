f2225ee79e4fdbb1b37026ea38ebc7f7e79d1dc7
lzxm160 <lzxm160@sina.com>
2019-07-30 13:28:57 +0800

    x

--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import (
 	"github.com/lzxm160/csvtomysql/utils"
 	"flag"
 	"fmt"
+	"os"
 )
 type config struct {
 	MysqlConnectString string`json:"mysqlConnectString,omitempty"`
 }
 
-func main() {
-	var configPath string
+var(
+	configPath string
+	csvPath string
+)
+
+func init() {
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(os.Stderr,
+			"usage: server -config=[string]\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
 	flag.StringVar(&configPath, "config", "config.toml", "path of config file")
+	flag.StringVar(&csvPath, "csv", "config.toml", "path of config file")
 	flag.Parse()
+}
+func main() {
 	var cfg config
 	utils.LoadConfig(configPath,&cfg)
 	fmt.Println("::",cfg)
