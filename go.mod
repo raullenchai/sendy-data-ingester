b992b3cb7e6d9beec4c2a19e56f38113d48e511f
lzxm160 <lzxm160@sina.com>
2019-07-30 14:05:48 +0800

    x

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lzxm160/csvtomysql
 
 go 1.12
+
+require github.com/BurntSushi/toml v0.3.1
--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"database/sql"
 	"encoding/csv"
-	"github.com/lzxm160/csvtomysql/utils"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/lzxm160/csvtomysql/utils"
 )
+
 type config struct {
-	MysqlConnectString string`json:"mysqlConnectString,omitempty"`
-	DbName string`json:"dbName,omitempty"`
-	TableName string`json:"tableName,omitempty"`
+	MysqlConnectString string `json:"mysqlConnectString,omitempty"`
+	DbName             string `json:"dbName,omitempty"`
+	TableName          string `json:"tableName,omitempty"`
 }
 
-
-var(
-	configPath string
-	csvPath string
-	line uint64
+var (
+	configPath   string
+	csvPath      string
+	line         uint64
 	sleepSeconds uint64
 )
 
@@ -33,20 +34,20 @@ func init() {
 	}
 	flag.StringVar(&configPath, "config", "config.toml", "path of config file")
 	flag.StringVar(&csvPath, "csv", "csv.csv", "path of csv file")
-	flag.Uint64Var(&line, "line",500000, "line")
-	flag.Uint64Var(&sleepSeconds, "sleep",3, "sleep")
+	flag.Uint64Var(&line, "line", 500000, "line")
+	flag.Uint64Var(&sleepSeconds, "sleep", 3, "sleep")
 	flag.Parse()
 }
-func openDB(cfg config)(*sql.DB,error){
+func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("mysql", cfg.MysqlConnectString+cfg.DbName+"?autocommit=false")
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return db,nil
+	return db, nil
 }
-func readAndWrite(s *sql.DB){
+func readAndWrite(s *sql.DB) {
 	f, err := os.Open(csvPath)
-	if err!=nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -62,21 +63,21 @@ func readAndWrite(s *sql.DB){
 			return
 		}
 
-		fmt.Println(record)
-		fmt.Println(len(record))
+		//fmt.Println(record)
+		//fmt.Println(len(record))
 		for value := range record {
-			fmt.Printf("  %v\n", record[value])
+			fmt.Printf(" value:%v %v\n", value, record[value])
 		}
 	}
 }
 func main() {
 	var cfg config
-	utils.LoadConfig(configPath,&cfg)
-	fmt.Println("config:",cfg)
-	db,err:=openDB(cfg)
+	utils.LoadConfig(configPath, &cfg)
+	fmt.Println("config:", cfg)
+	db, err := openDB(cfg)
 	defer db.Close()
 
-	if err!=nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
